f2dc6848d251f007eaa404d24f54f7f3f3548920
MarcoRosselDev <andresmarcorossel@gmail.com>
2022-12-20 16:03:07 -0300

    class 3.4

--- app.js
+++ app.js
@@ -3,17 +3,21 @@ const canvas = document.getElementById("canvas1");
 const ctx = canvas.getContext("2d");
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-
-numberOfEnemies = 200;
+numberOfEnemies = 10;
 enemyArray = [];
+
+const enemyImage = new Image();
+enemyImage.src = "enemy1.png";
 
 class Enemy {
   constructor() {
     this.x = Math.random() * canvas.width;
     this.y = Math.random() * canvas.height;
-    this.width = 100;
-    this.height = 100;
     this.speed = Math.random() * 4 - 2;
+    this.spriteWidth = 293;
+    this.spriteHeight = 155;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
   }
   update() {
     this.x += this.speed;
@@ -21,6 +25,17 @@ class Enemy {
   }
   draw() {
     ctx.strokeRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(
+      enemyImage,
+      0,
+      0,
+      this.spriteWidth,
+      this.spriteHeight,
+      this.x,
+      this.y,
+      this.width,
+      this.height
+    );
   }
 }
 
